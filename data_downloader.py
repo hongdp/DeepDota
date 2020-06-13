18ba2413de5854a4965c3c97c92422a7aa1cbe46
hongdp <hongdp@gmail.com>
2020-06-13 16:32:18 -0700

    Add a baseline model.

--- data_downloader.py
+++ data_downloader.py
@@ -4,10 +4,10 @@ import pickle
 from functools import partial
 from multiprocessing import Pool
 
-NUM_MATCHES = 5000
-MATCH_LIST_FILE = 'match_data.binary'
-MATCH_DETAILS_FILE = 'match_detail.binary'
-DOWNLOAD_MATCH_LIST = False
+NUM_MATCHES = 50000
+MATCH_LIST_FILE = 'match_data_50000.binary'
+MATCH_DETAILS_FILE = 'match_detail_50000.binary'
+DOWNLOAD_MATCH_LIST = True
 DOWNLOAD_MATCH_DETAILS = True
 API_KEY_FILE = 'API_KEY'
 
@@ -31,7 +31,6 @@ def download_match_list(num_matches, api_key):
 
 def download_match_detail(match, api_key):
     match_id = match['match_id']
-    print(api_key)
     res = requests.get(
         'https://api.opendota.com/api/matches/%d?api_key=%s' % (match_id, api_key))
     if res.status_code == 200:
@@ -49,7 +48,7 @@ def main():
 
     if DOWNLOAD_MATCH_LIST:
         match_list = download_match_list(NUM_MATCHES, api_key)
-        with open(MATCH_LIST_FILE, 'wb') as f:
+        with open(MATCH_LIST_FILE, 'w+b') as f:
             pickle.dump(match_list, f)
     else:
         with open(MATCH_LIST_FILE, 'rb') as f:
@@ -60,7 +59,7 @@ def main():
             download_match_detail, api_key=api_key)
         match_details = pool.map(download_match_detail_with_api, match_list)
 
-        with open(MATCH_DETAILS_FILE, 'wb') as f:
+        with open(MATCH_DETAILS_FILE, 'w+b') as f:
             pickle.dump(match_details, f)
 
 
--- train.py
+++ train.py
@@ -5,14 +5,20 @@ TRAINING_DATA_FILE = 'training_data.tfrecord'
 
 def _get_feature_parser():
     feature_description = {
-        'radiant_win': tf.io.FixedLenFeature([], tf.float32, default_value=0),
         'radiant_heros': tf.io.FixedLenFeature([5], tf.int64, default_value=[0]*5),
         'dire_heros': tf.io.FixedLenFeature([5], tf.int64, default_value=[0]*5),
     }
+    label_description = {'radiant_win': tf.io.FixedLenFeature(
+        [], tf.float32, default_value=0)}
 
     def _parse_function(example_proto):
         # Parse the input `tf.Example` proto using the dictionary above.
-        return tf.io.parse_single_example(example_proto, feature_description)
+        parsed_features = tf.io.parse_single_example(
+            example_proto, feature_description)
+        parsed_labels = tf.io.parse_single_example(
+            example_proto, label_description)
+
+        return parsed_features, parsed_labels['radiant_win']
 
     return _parse_function
 
@@ -21,15 +27,49 @@ class MyModel(tf.keras.Model):
 
     def __init__(self):
         super(MyModel, self).__init__()
-        self.dense1 = tf.keras.layers.Embedding(150, 32, )
+        # Embedding layer with size = 32.
+        embedding_size = 32
+        self.embed_radiant = tf.keras.layers.Embedding(
+            150, embedding_size, input_length=5)
+        self.embed_dire = tf.keras.layers.Embedding(
+            150, embedding_size, input_length=5)
+        self.dense_layers = []
+        self.dense_layers.append(
+            tf.keras.layers.Dense(embedding_size, activation=tf.nn.relu))
+        self.dense_layers.append(
+            tf.keras.layers.Dense(embedding_size//2, activation=tf.nn.relu))
+        self.dense_layers.append(
+            tf.keras.layers.Dense(1, activation=tf.nn.relu))
+
+    def call(self, inputs):
+        '''
+        inputs: {
+            'dire_heros': <tf.Tensor: id=66, shape=(5,), dtype=int64, numpy=array([ 2, 72, 25, 87, 11])>,
+            'radiant_heros': <tf.Tensor: id=67, shape=(5,), dtype=int64, numpy=array([ 91,  52, 104,  86,  17])>,
+            'radiant_win': <tf.Tensor: id=68, shape=(), dtype=float32, numpy=0.0>
+        }
+        '''
+        embed_radiant_val = tf.math.reduce_sum(
+            self.embed_radiant(inputs['radiant_heros']), axis=1)
+        embed_dire_val = tf.math.reduce_sum(
+            self.embed_dire(inputs['dire_heros']), axis=1)
+        print(embed_dire_val)
+
+        x = tf.concat([embed_radiant_val, embed_dire_val], axis=1)
+        print(x)
+        for dense_layer in self.dense_layers:
+            x = dense_layer(x)
+        return x
 
 
 def main():
     raw_dataset = tf.data.TFRecordDataset(TRAINING_DATA_FILE)
     parsed_dataset = raw_dataset.map(_get_feature_parser())
-    for parsed_record in parsed_dataset.take(10):
-        print('taking')
-        print(repr(parsed_record))
+    dataset = parsed_dataset.batch(128).repeat(100)
+    model = MyModel()
+    model.compile(optimizer=tf.keras.optimizers.Adam(0.01),
+                  loss=tf.keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+    model.fit(dataset)
 
 
 if __name__ == '__main__':
